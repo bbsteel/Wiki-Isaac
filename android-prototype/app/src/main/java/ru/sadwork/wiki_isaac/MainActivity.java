4323cdba2435681c56a0ca1096867c8726d842b3
Andrey Lysenkov <andreylysenkovallan@gmail.com>
2016-08-26 09:54:38 +0300

    [test] add log on search

--- android-prototype/app/src/main/java/ru/sadwork/wiki_isaac/MainActivity.java
+++ android-prototype/app/src/main/java/ru/sadwork/wiki_isaac/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +14,8 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String LOG_TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void onClickSearchButton(View v){
-
+        Log.d(LOG_TAG, "search");
     }
 
     @Override
